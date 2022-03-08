bd4d20092ea03f7999a2963deb8f95d16157b53d
ilyasher <ilyasherstyuk@gmail.com>
2022-03-07 19:59:27 -0800

    Abstract showgrid into utils.py

--- many_astar.py
+++ many_astar.py
@@ -17,60 +17,8 @@ from utils import *
 #   will be the columns (to the right) and the Y-axis will be the rows
 #   (top downward).
 #
-def showgrid(state):
 
-    # Grab the dimensions.
-    M = np.size(state, axis=0)
-    N = np.size(state, axis=1)
-
-    # Close the old figure.
-    plt.close()
-
-    # Create the figure and axes.
-    fig = plt.figure()
-    ax  = plt.axes()
-
-    # turn off the axis labels
-    ax.axis('off')
-
-    # Draw the grid, zorder 1 means draw after zorder 0 elements.
-    for m in range(M+1):
-        ax.axhline(m, lw=1, color='b', zorder=1)
-    for n in range(N+1):
-        ax.axvline(n, lw=1, color='b', zorder=1)
-
-    # Create the color range.  There are clearly more elegant ways...
-    color = np.ones((M,N,3))
-    for m in range(M):
-        for n in range(N):
-            state_to_color = {
-                WALL: 'black',
-                UNKNOWN: 'white',
-                ONDECK: 'white',
-                PROCESSED: 'lightblue',
-                ONPATH: 'purple',
-                START: 'green',
-                GOAL: 'red',
-                PATH_STATES[0]: 'purple',
-                PATH_STATES[1]: 'orange',
-                PATH_STATES[2]: 'magenta',
-                PATH_STATES[3]: 'yellow',
-                PATH_STATES[4]: 'maroon'
-            }
-            if state[m, n] not in state_to_color:
-                c = 'red'
-            else:
-                c = state_to_color[state[m, n]]
-            color[m,n,0:3] = np.array(matplotlib.colors.to_rgb(c))
-
-    # Draw the boxes
-    ax.imshow(color, aspect='equal', interpolation='none',
-              extent=[0, N, 0, M], zorder=0)
-
-    # Force the figure to pop up.
-    plt.pause(0.001)
-
-mappath = sys.argv[1] if len(sys.argv) > 1 else 'maps/map1.txt'
+mappath = sys.argv[1] if len(sys.argv) > 1 else 'maps/hw1_many_robots.txt'
 state, robots_start, robots_goal = load_map(mappath)
 M, N = state.shape
 
@@ -175,7 +123,6 @@ def a_star_multi(state, starts, goals, a_factor=1, visualize=False):
             return path
 
         # Add all unseen neighbors to the processing queue
-        #print(coords)
         if visualize:
             state_show = state.copy()
             for i, robot_loc in enumerate(coords):
@@ -184,7 +131,6 @@ def a_star_multi(state, starts, goals, a_factor=1, visualize=False):
         assert coords not in visited
         visited.add(coords)
         print(len(visited), len(q), len(set(q)), len(coords), len(get_neighbors_multi(coords)))
-        #print(q)
         for nbor in get_neighbors_multi(coords):
             if nbor in visited:
                 continue
@@ -197,29 +143,18 @@ def a_star_multi(state, starts, goals, a_factor=1, visualize=False):
             heappush(q, (len(starts) + dist + a_factor * man_dist, nbor))
             parents[nbor] = coords
 
-        # Add the option of remaining in the same location
-        #new_coord = SpaceTimeCoordinate(coord.x, coord.y, coord.time + 1)
-        #man_dist = manhattan_distance(goal, new_coord.get_space())
-        #heappush(q, (1 + dist + new_coord.time + a_factor * man_dist, new_coord))
-        #parents[new_coord] = coord
-
     # Never reached
     raise RuntimeError("Error: should never be reached")
 
-def dijkstra_multi(state, start, goal):
-    return a_star_multi(state, start, goal, a_factor=0)
+for i in range(len(robots_start)):
+    state[robots_start[i]] = PATH_STATES[i]
+    state[robots_goal[i]]  = GOAL
 
-PATH_COLORS = ['purple', 'orange', 'yellow', 'magenta', 'maroon']
-
-for start, end in zip(robots_start, robots_goal):
-    state[start] = START
-    state[end]   = GOAL
-
-state_save = state.copy()
 path = a_star_multi(state, robots_start, robots_goal, a_factor=2)
 for step in path:
-    state_show = state_save.copy()
     for i, robot_loc in enumerate(step):
-        state_show[robot_loc] = PATH_STATES[i]
-    showgrid(state_show)
+        state[robot_loc] = PATH_STATES[i]
+    showgrid(state)
+    for i, robot_loc in enumerate(step):
+        state[robot_loc] *= -1
     input('Hit return to continue')
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 
 WALL      = 0
 UNKNOWN   = 1
@@ -39,3 +41,61 @@ def load_map(filepath):
         robots_goal.append(goals[robot])
 
     return state, tuple(robots_start), tuple(robots_goal)
+
+def showgrid(state):
+
+    # Grab the dimensions.
+    M = np.size(state, axis=0)
+    N = np.size(state, axis=1)
+
+    # Close the old figure.
+    plt.close()
+
+    # Create the figure and axes.
+    fig = plt.figure()
+    ax  = plt.axes()
+
+    # turn off the axis labels
+    ax.axis('off')
+
+    # Draw the grid, zorder 1 means draw after zorder 0 elements.
+    for m in range(M+1):
+        ax.axhline(m, lw=1, color='b', zorder=1)
+    for n in range(N+1):
+        ax.axvline(n, lw=1, color='b', zorder=1)
+
+    # Create the color range.  There are clearly more elegant ways...
+    color = np.ones((M,N,4))
+    is_path = state < 0
+    state = np.abs(state)
+    for m in range(M):
+        for n in range(N):
+            state_to_color = {
+                WALL: 'black',
+                UNKNOWN: 'white',
+                ONDECK: 'white',
+                PROCESSED: 'lightblue',
+                ONPATH: 'purple',
+                START: 'green',
+                GOAL: 'red',
+                PATH_STATES[0]: 'purple',
+                PATH_STATES[1]: 'orange',
+                PATH_STATES[2]: 'magenta',
+                PATH_STATES[3]: 'yellow',
+                PATH_STATES[4]: 'maroon'
+            }
+            if state[m, n] not in state_to_color:
+                c = 'red'
+            else:
+                c = state_to_color[state[m, n]]
+            c = matplotlib.colors.to_rgba_array(c).flatten()
+            if is_path[m, n]:
+                c[-1] = 0.5
+            color[m,n] = c
+
+    # Draw the boxes
+    ax.imshow(color, aspect='equal', interpolation='none',
+              extent=[0, N, 0, M], zorder=0)
+
+    # Force the figure to pop up.
+    plt.pause(0.001)
